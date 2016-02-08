e7c6285acea4b7f583ed1de465eadbec4a3e242a
3uch <devil.playboys@gmail.com>
2016-02-08 19:59:12 +0700

    Added arrayList

---
+++ ArrayList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            ArrayList array = new ArrayList();
+            Console.WriteLine("array size: " + array.Count);
+            for(int i = 0; i < 7; i++)
+            {
+                array.Add(i * 2);
+            }
+            
+            Console.WriteLine("array size: " + array.Count);
+            
+            printArray(array);
+            for (int j = 7; j < 10; j++)
+            {
+                array.Add(j * 3);
+            }
+            Console.WriteLine();
+            Console.WriteLine("array size: " + array.Count);
+
+            printArray(array);
+
+            Console.ReadKey();
+        }
+
+        public static void printArray(ArrayList a)
+        {
+            for (int i = 0; i < a.Count; i++)
+            {
+                Console.Write(a[i]+" ");
+            }
+        }
+    }
+}
