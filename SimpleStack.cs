fadf3617a4aec6339a0cdac528d40333a03d9deb
3uch <devil.playboys@gmail.com>
2016-02-08 22:33:09 +0700

    Added SimpleStack and TestStack file

---
+++ SimpleStack.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace ConsoleApplication2
+{
+    public class SimpleStack
+    {
+        private int stackSize;
+        private int top;
+        private Object[] stack;
+
+        public SimpleStack(int size)
+        {
+            stackSize = size;
+            top = 0;
+            stack = new Object[stackSize];
+        }
+
+        public void push(object data)
+        {
+            if(top + 1 < stackSize){
+                top++;
+                stack[top] = data;
+            }
+            else {
+                throw new Exception("Stack Overflow");
+            }
+        }
+
+        public Object pop()
+        {
+            if(top > 0)
+            {
+                Object data = stack[top];
+                top--;
+                return data;
+            }
+            else
+            {
+                throw new Exception("Stack Underflow");
+            }
+        }
+
+        public int count()
+        {
+            if (top > 0)
+            {
+                return top;
+            }
+            if(top < 1)
+            {
+                return 0;
+            }
+            else
+            {
+                throw new Exception("Stack Underflow");
+            }
+        }
+    }
+}
---
+++ TestStack.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ConsoleApplication2
+{
+    class TestStack
+    {
+        public static void Main(string[] args)
+        {
+            SimpleStack stack = new SimpleStack(5);
+            Console.WriteLine("============ Push data into stack ============" );
+            for(int i = 1; i < 5; i++)
+            {
+                Console.WriteLine("Push :" + i);
+                stack.push(i);
+            }
+
+            Console.WriteLine("============ Pop data into stack ============");
+            for(int j = 1; j < 5; j++)
+            {
+                Console.WriteLine("Pop = {0}", stack.pop());
+            }
+            Console.ReadKey();
+        }
+    }
+}
